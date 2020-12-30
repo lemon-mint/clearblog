b4a1c670f77543351d6d212529b49db52074012a
lemon-mint <55233766+lemon-mint@users.noreply.github.com>
2020-12-30 18:07:13 +0900

    Try the template

--- main.go
+++ main.go
@@ -1,16 +1,63 @@
 package main
 
 import (
-	"net/http"
+	"encoding/json"
+	"html/template"
+	"io"
+	"io/ioutil"
+	"os"
 
 	"github.com/labstack/echo/v4"
 )
 
+var config = func() map[string]string {
+	config, err := func() (map[string]string, error) {
+		configfile, err := os.Open("config.json")
+		if err != nil {
+			return map[string]string{}, err
+		}
+		jsondata, err := ioutil.ReadAll(configfile)
+		if err != nil {
+			return map[string]string{}, err
+		}
+		data := make(map[string]string)
+		err = json.Unmarshal(jsondata, &data)
+		if err != nil {
+			return map[string]string{}, err
+		}
+		return data, nil
+	}()
+	if err != nil {
+		os.Exit(-1)
+	}
+	return config
+}()
+
+//Template wraper
+type Template struct {
+	templates *template.Template
+}
+
+//Render template
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
 func main() {
+	t := &Template{
+		templates: template.Must(template.ParseGlob("views/*.html")),
+	}
 	e := echo.New()
+	e.Renderer = t
 	e.Static("/static", "public")
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+		return c.Render(
+			200,
+			"index.html",
+			map[string]interface{}{
+				"PageTitle": config["site.title"],
+			},
+		)
 	})
 	e.Logger.Fatal(e.Start(":1323"))
 }
